c954923adc8070cb8eaf494ed344b97605e151dd
Sho Hashimoto <hashimoto@shokai.org>
2016-09-16 16:25:44 +0900

    fix test description

--- test/util.js
+++ test/util.js
@@ -4,7 +4,7 @@ import {uniq} from '../src/util'
 import {assert} from 'chai'
 
 describe('Util', function () {
-  describe('uniq', function () {
+  describe('uniq(arr)', function () {
     let result = uniq([1, 3, 2, 3, 1, 1, 3, 4, 2])
     it('filter duplicated item',
        () => assert.deepEqual(result.sort(), [1, 2, 3, 4]))
