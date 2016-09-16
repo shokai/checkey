9414ceb09a929d0284e36afd599cedc7a9a9d8d3
Sho Hashimoto <hashimoto@shokai.org>
2016-09-16 15:53:43 +0900

    update sample

--- sample/sample.js
+++ sample/sample.js
@@ -1,10 +1,10 @@
 // var checkey = require('checkey')
 var checkey = require('../')
 
-// define schema which require "name", permit "name" & "web"
+// define schema which require "name", permit "name" & "web" & "mail"
 var schema = {
   require: [ 'name' ],
-  permit: [ 'web' ]
+  permit: [ 'web', 'mail' ]
 }
 
 // valid object
@@ -13,16 +13,20 @@ var result1 = checkey({
 }, schema)
 console.log('result1 is valid => ', result1.isValid)
 
-// invalid object
+// valid object
 var result2 = checkey({
-  foo: 'bar'
+  name: 'shokai',
+  web: 'http://shokai.org',
+  mail: 'hashimoto@shokai.org'
 }, schema)
 console.log('result2 is valid => ', result2.isValid)
-console.log(result2.errors)
 
-// valid object
+// invalid object ... "name" is required, "fullname" is not pemited
 var result3 = checkey({
-  name: 'shokai',
-  web: 'http://shokai.org'
+  fullname: 'Sho Hashimoto',
+  web: 'http://shokai.org',
+  mail: 'hashimoto@shokai.org'
 }, schema)
 console.log('result3 is valid => ', result3.isValid)
+console.log(result3.errors)
+
